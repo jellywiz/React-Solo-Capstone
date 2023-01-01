feb9619f7931193d722cc36c4a6a92ca6d049d71
jellywiz <muzaffer.dler@gmail.com>
2023-01-01 17:14:28 +0300

    Add cryptoSlice file with the implementation to feth the data from the api

---
+++ src/Redux/cryptoSlice.js
@@ -0,0 +1,22 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const getData = createAsyncThunk('crypto/getData', async () => {
+  const response = await fetch('https://api.coinstats.app/public/v1/coins/');
+  const dataList = await response.json();
+  const { coins } = dataList;
+  return coins;
+});
+
+const initialState = [];
+const cryptoSlice = createSlice({
+  name: 'crypto',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getData.fulfilled, (state, action) => action.payload);
+  },
+});
+
+const { actions, reducer } = cryptoSlice;
+export { actions, getData };
+export default reducer;
