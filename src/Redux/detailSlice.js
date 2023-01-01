257f20180cfab75859bdc0272f2e91cf187dd902
jellywiz <muzaffer.dler@gmail.com>
2023-01-01 17:18:22 +0300

    Add detailed slice and crypto slice

--- src/Redux/detailSlice.js
+++ src/Redux/detailSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const getDetail = createAsyncThunk('crypto/Detail', async (id) => {
   const response = await fetch(
-    `https://api.coinstats.app/public/v1/coins/${id}`,
+    `https://api.coinstats.app/public/v1/coins/${id}`
   );
   const detail = await response.json();
   const { coin } = detail;
