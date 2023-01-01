0c9ee7075411d923da8243aef3a21fd4d49a671d
jellywiz <muzaffer.dler@gmail.com>
2023-01-01 22:49:05 +0300

    Create navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import BackIcon from '../img/backimg.jpg';
 
-function Navbar() {
-  return <div>Navbar</div>;
+function Navbar({ back = false }) {
+  return (
+    <nav className="">
+      {back ? (
+        <Link to="/React-Solo-Capstone/" className="NavBar__Back">
+          <img src={BackIcon} alt="" />
+        </Link>
+      ) : (
+        ''
+      )}
+    </nav>
+  );
 }
 
 export default Navbar;
+
+Navbar.defaultProps = {
+  back: false,
+};
+
+Navbar.propTypes = {
+  back: PropTypes.bool,
+};
