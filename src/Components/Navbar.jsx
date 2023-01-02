e32019509ccf303f1e9d1c1fa758360bf8246cab
jellywiz <muzaffer.dler@gmail.com>
2023-01-02 10:21:04 +0300

    Add styling into the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import BackIcon from '../img/backimg.jpg';
+import logoNav from '../img/proton-xpr-logo.svg';
+import '../style/Navbar.css';
 
 function Navbar({ back = false }) {
   return (
-    <nav className="">
-      {back ? (
-        <Link to="/React-Solo-Capstone/" className="NavBar__Back">
-          <img src={BackIcon} alt="" />
-        </Link>
-      ) : (
-        ''
-      )}
-    </nav>
+    <div>
+      <nav className="">
+        {back ? (
+          <Link to="/React-Solo-Capstone/" className="NavBar__Back">
+            <img src={BackIcon} alt="" />
+          </Link>
+        ) : (
+          ''
+        )}
+      </nav>
+      <div className="Navbar-header">
+        <h1>Crypto</h1>
+        <img src={logoNav} alt="" />
+        <h1>World!</h1>
+      </div>
+    </div>
   );
 }
 
